b05028c4004f91acd79a15183b10c9acc4aa7b81
KO <j316ko@naver.com>
2024-04-25 11:12:14 +0900

    123

--- 003.JS학습/01.JS실험실/js/call_myself.js
+++ 003.JS학습/01.JS실험실/js/call_myself.js
@@ -74,7 +74,7 @@ function moveGallary(){
 
 }/////////moveGallery//////////
 
-setTimeout(moveGallary,1000);/*moveGallary호출!! 시작 지연 1000=1초 */
+// setTimeout(moveGallary,1000);/*moveGallary호출!! 시작 지연 1000=1초 */
 
 
 
@@ -95,5 +95,45 @@ mFn.addEvt(gbox,"mouseleave",()=>{
     //멈춤상태변수 false변경
     stopSts=false;
     //재귀호출함수 호출!
-    moveGallary();
+    // moveGallary();
 });
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////
+///프로그래스 바 '퍼센트' 증가하기 재귀호출함수 만들기////
+//퍼센트 증가 숫자변수
+let percent=0;
+// 숫자출력박스: .pNum
+const pNum =mFn.qs(".pNum");
+// 퍼센트바: .bar
+const bar=mFn.qs(".bar");
+// 준비글자: .txt
+const txt=mFn.qs(".txt");
+
+// 재귀호출함수 만들기
+function increasePercent(){
+    //1.pNum에 숫자출력
+    pNum.innerHTML= ++percent+"%";
+    //2.퍼센트바 width값 동시에 증가하기
+    bar.style.width=percent+"%";
+
+    //3.증가숫자가 100보다 작을때까지 계속 재귀호출
+    if(percent<100)
+    {setTimeout(increasePercent,60);}
+
+    else{
+    //4.재귀호출이 끝나면 "준비"글자를 "출발!"로 변경
+    txt.innerHTML = "출발!"    
+    bar.style.backgroundColor ="red";
+    //5.슬라이드 이동함수 호출하기(타임아웃호출은 주석)
+        moveGallary();
+    }//////else/////
+}/////increasePercent함수///////'
+
+/* 퍼센트바 호출~ */
+increasePercent();
