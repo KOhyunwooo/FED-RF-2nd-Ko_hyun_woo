14d79d2da8f3bfe8dfefee5b2beadbe991cf7b28
KO <j316ko@naver.com>
2024-05-09 18:27:11 +0900

    123

--- 003.JS학습/js/js4-4.array_sort.js
+++ 003.JS학습/js/js4-4.array_sort.js
@@ -95,6 +95,52 @@ import mFn from "./my_function.js";
     예) 특수문자 x변수를 y변수와 변환후 비교 
         x.localeCompare(y)
 
+*************************************************************
+
+      [ 배열의 검색 : find() / filter() / indexOf() ]
+
+      1. find() 메서드
+      (1) 검색후 첫번째 일치값 하나만을 리턴
+      (2) 결과가 없으면 undefined 리턴(if문에서 false처리!)
+      (3) 콜백함수구성 : function(val,idx,obj){}
+          1) val : 처리중 배열의 값
+          2) idx : 처리중 배열의 순번
+          3) obj : 처리중 배열전체
+      (4) 리턴데이터 : 배열의 값 하나(값의 데이터형)
+      (5) 활용케이스 : 아이디검사 결과 리턴
+      (6) 코드예 :
+          변수 = 배열.find(v=>{
+              if(v[속성명].indexOf(검색어)!==-1) return true;
+          })
+          -> 배열을 자동순회하여 if문에 해당되는 데이터가 있으면
+          return true 하여 할당된 변수에 저장하고 문장이 바로 끝난다!
+
+
+      2. filter() 메서드
+      (1) 검색후 모든 일치값을 리턴
+      (2) 결과가 없으면 빈배열 리턴([]->배열.length 값이 0)
+      (3) 콜백함수구성 : function(val,idx,obj){}
+          1) val : 처리중 배열의 값
+          2) idx : 처리중 배열의 순번
+          3) obj : 처리중 배열전체
+      (4) 리턴데이터 : 배열형데이터(하나여도 배열값으로 넘어옴!)
+      (5) 활용케이스 : 검색리스트 결과 리턴
+      (6) 코드예 :
+          변수 = 배열.filter(v=>{
+              if(v[속성명].indexOf(검색어)!==-1) return true;
+          })
+          -> 배열을 자동순회하여 if문에 해당되는 데이터가 있으면
+          return true 하여 다른값이 계속 나올때까지 배열로 값을
+          할당변수에 저장한다!(배열을 전체순회함!)
+
+      3. LIKE 검색방법 : 값의 일부만 넣어도 검색되는 방법
+      -> indexOf(값) 을 사용함!
+      결과값으로 문자열의 위치순번을 리턴하는데
+      만약 없으면 -1을 리턴하므로 이것을 이용하여 
+      조건문에 -1이 아닌경우가 검색결과가 있는 경우가 됨!
+      예) 
+      if(문자열.indexOf(검색문자열)!==-1){결과리턴}
+
 ******************************************************/
 
 // 숫자값 배열
@@ -387,12 +433,11 @@ const cta3 = mFn.qs(".cta3");
 // (3) 정렬종류 대상 선택 변경시
 // -> 실제 정렬을 적용하여 리스트를 갱신한다!
 // -> 정렬 적용시 정렬기준 대상 선택항목을 가져가야함!
-mFn.addEvt(sel3,"change",
-(e)=>sortingFn(e,cta3.value,list1,showList3));
+mFn.addEvt(sel3, "change", (e) => sortingFn(e, cta3.value, list1, showList3));
 
 // (4) 정렬기준 대상 선택 변경시
 // -> 정렬종류 대상 초기화하기("정렬선택"으로 변경!)
-mFn.addEvt(cta3,"change",()=>{
+mFn.addEvt(cta3, "change", () => {
   // 정렬종류 첫번째 값은 value가 "0"이므로
   // 이것을 value 에 할당하면 선택박스값이 첫번째로 변경된다!
   sel3.value = "0";
@@ -408,29 +453,119 @@ function sortingFn(evt, cta, arrData, exBox) {
 
   // 1. 선택값 읽어오기(오름차순:1,내림차순:2)
   let selVal = evt.target.value;
-  console.log("선택값:",selVal);
+  console.log("선택값:", selVal);
 
   // 검색기준 선택박스 값 읽어오기
-  console.log("정렬기준:",cta);
+  console.log("정렬기준:", cta);
 
   // 2. 정렬분기하기 ////////////////
   // 2-1. 오름차순
-  if(selVal == 1){
-    arrData.sort((a,b)=> 
-    a[cta] == b[cta] ? 0 : a[cta] > b[cta] ? 1 : -1);
-    
+  if (selVal == 1) {
+    arrData.sort((a, b) => (a[cta] == b[cta] ? 0 : a[cta] > b[cta] ? 1 : -1));
   } /// if /////
   // 2-2. 내림차순
-  else if(selVal == 2){
-    arrData.sort((a,b)=> 
-    a[cta] == b[cta] ? 0 : a[cta] > b[cta] ? -1 : 1);
-
+  else if (selVal == 2) {
+    arrData.sort((a, b) => (a[cta] == b[cta] ? 0 : a[cta] > b[cta] ? -1 : 1));
   } /// else if /////
 
-  console.log("정렬결과:",arrData);
+  console.log("정렬결과:", arrData);
 
   // 3. 정렬결과 리스트 업데이트하기
-  updateCode(arrData,exBox); 
-
+  updateCode(arrData, exBox);
 } ////////////// sortingFn 함수 ////////////////
 
+//////////////////////////////////////////////
+////////////// 배열의 검색 !!! ////////////////
+//////////////////////////////////////////////
+
+// 4. 객체데이터 검색후 배열의 정렬 ////////////
+
+// 4-1. 출력대상선정: showList4
+const showList4 = mFn.qs(".showList4");
+// console.log(showList4);
+
+// 4-2. 데이터셋팅 : 객체 데이터 배열
+const list2 = [
+  {
+    idx: 58,
+    tit: "당근마켓에 가자",
+    cont: "당근마켓이 항상 좋은건 아니야~!!ㅠ.ㅠ",
+  },
+  {
+    idx: 15,
+    tit: "당근마켓에 가자",
+    cont: "당근마켓이 정말로 싸고 좋다구~!",
+  },
+  {
+    idx: 74,
+    tit: "점심에 뭐먹지? 당근이지!",
+    cont: "오스틴님 생일 서포트 안내",
+  },
+  {
+    idx: 18,
+    tit: "직돌이는 쉬고싶다~!",
+    cont: "활동정지에 대한 파생글 무통보 삭제 및 경고",
+  },
+  {
+    idx: 104,
+    tit: "올해는 다른 회사로 이직한다!",
+    cont: "⚜️갈라콘 서포트에 많은 참여 부탁드립니다!",
+  },
+]; /////////////// list2 /////////////
+
+// 4-3. 코드 만들어 출력하는 함수 호출하기
+// updateCode(배열데이터,출력박스)
+updateCode(list2, showList4);
+
+// 4-5. 검색 이벤트 설정하기 /////////
+// 4-5-1. 대상선정 :
+// (1) 검색기준 선택박스
+const searchCta4 = mFn.qs(".search-cta4");
+// (2) 검색버튼
+const btnSearch = mFn.qs(".sbtn");
+// (3) 검색어 입력창
+const keyWord = mFn.qs("#stxt");
+// console.log(searchCta4,btnSearch,keyWord);
+
+// 4-5-2. 이벤트 설정하기 ////
+mFn.addEvt(btnSearch, "click", searchingFn);
+
+// 4-6. 검색함수 만들기 ////////////////
+function searchingFn() {
+  // 1. 검색 기준값 읽어오기
+  let cta = searchCta4.value;
+  // 2. 검색어 읽어오기
+  let kword = keyWord.value;
+  // 3. 검색어가 없으면 돌아가!
+  if (kword == "") {
+    alert("검색어를 입력해주세요!");
+    // 입력창에 포인터 들어가기! focus()
+    keyWord.focus();
+    return;
+  }
+  console.log(cta, kword);
+
+  // 4. 검색기준으로 검색어를 사용하여 검색하기
+  // 검색대상 데이터 배열 : list2
+  // 사용 배열메서드 : filter()
+  let result = list2.filter((v) => {
+    // v는 배열값
+    // 만약에 찾는문자가 전체문자열에 있으면 -1이 아님!!!
+    // -> 숫자이면 에러남! 왜? indexOf()는 문자열전문!
+    // 무조건 문자형으로 변환한다! String(대상)
+    if (String(v[cta]).indexOf(kword) != -1) return true;
+    // 이 조건에 리턴값을 true로 하면 해당 데이터를
+    // 배열로 만들어서 순서대로 변수에 할당한다!
+    // 여기서는 result변수가 결과 배열 변수가 된다!
+    // console.log(v["tit"].indexOf(kword));
+  });
+  // 전체문자열.indexOf(문자열) -> 해당문자열이 전체문자열에서
+  // 몇번째에 있는지 그 순번을 리턴해 주는 메서드다!
+  // 만약 없으면 -1값을 리턴한다!!!
+
+  // 결과찍기
+  console.log(result);
+
+  // 5. 결과를 화면에 보여주기 : updateCode 함수호출
+  updateCode(result, showList4);
+} ////////////// searchingFn 함수 ///////////
