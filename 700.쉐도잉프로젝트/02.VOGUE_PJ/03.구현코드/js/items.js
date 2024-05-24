c3911c7cc7e29aef1ebc91f98b42e79a90776935
KO <j316ko@naver.com>
2024-05-24 17:51:02 +0900

    123123

---
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/items.js
@@ -0,0 +1,36 @@
+// 보그 PJ 메인 JS - items.js
+
+//상단영역 불러오기
+import TopArea from "./components/TopArea";
+
+//아이템영역 불러오기
+import ItemsArea from "./components/ItemsArea";
+
+//하단영역 불러오기
+import FooterArea from "./components/FooterArea";
+
+// [1] 아이템 페이지 전체 레이아웃 로딩 컴포넌트 ///
+function Layout() {
+    // 리턴코드구역
+    return (
+           <React.Fragment>
+                {/* 1. 상단영역 컴포넌트 */}
+                <TopArea />
+                {/* 2. 메인영역 컴포넌트 */}
+                <ItemsArea catName="video"/>
+                {/* 3. 하단영역 컴포넌트 */}
+                <FooterArea />
+           </React.Fragment>
+    );
+} ///////// LayOut 컴포넌트 /////////
+
+// 상단영역 출력하기///////////////////////////////
+// ReactDOM.render(어쩌구,저쩌구);
+// ReactDOM.render(<TopArea />,document.querySelector("#top-area"));
+// 메인영역 출력하기//////////////////////////////
+// ReactDOM.render(<MainArea/>,document.querySelector("#main-area"));
+//하단영역 출력하기///////////////////////
+// ReactDOM.render(<FooterArea/>, document.querySelector("#footer-area"));
+// ↓
+// 메인 페이지(상단영역, 메인영역, 하단영역) 전체 출력하기
+ReactDOM.render(<Layout/>, document.querySelector("#root"));
