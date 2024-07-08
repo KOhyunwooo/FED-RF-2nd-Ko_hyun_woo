fdd69cd6400b91b9b67a76dc333e028e3c279906
KO <j316ko@naver.com>
2024-07-08 12:30:40 +0900

    124

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
@@ -1,81 +1,110 @@
 // 오피니언 페이지 컴포넌트 ///
-
 import { Fragment, useRef, useState } from "react";
-// 사용자 기본정보 생성함수(회원가입할떄 썻던거) 불러오기
-import { initData } from "../func/mem_fn";
 
-// 제이쿼리 불러오기
+// 사용자 기본정보 생성 함수
+// import { initData } from "../func/mem_fn";
+
+// 로컬스토리지 게시판 기본데이터 제이슨 -> 로컬쓰로 대체!!!
+// import baseData from "../data/board.json";
+// 리액트 웹펙에서 제이슨은 이름을 지어서 불러오면된다!
+// 제이슨 파일 처리는 다르므로 확장자는 반드시 씀!
+
+// 제이쿼리
 import $ from "jquery";
 
-// 게시판용 css 불러오기
+// 게시판용 CSS
 import "../../css/board.scss";
 import "../../css/board_file.scss";
 
-// 로컬스토리지 게시판 기본데이터 제이슨 불러오기
-// 제이슨 파일 불러오는 방법 import baseData from ""  로 찾는다. baseData는 내가 짓는 이름
-import baseData from "../data/board.json";
-
-// 리액트 웹팩에서 제이슨 이름을 지어서 불러오면됨.
-// 제이슨 파일 처리는 다르므로 확장자는 반드시 씀!
+// 로컬스토리지 확인 JS
+import { initBoardData } from "../func/board_fn";
 
 export default function Board() {
-    // [상태관리 변수]/////////////////
-    // [1] 페이지 번호
-    // pageNum을 상태관리 변수로 만들어서 pageNum을 변경할 수 있게 만들기
-    const [pgNum, setPgNum] = useState(1);
+    // 로컬스토리지 게시판 데이터 정보확인! //
+    initBoardData();
 
-    // [참조변수]////////////////////
-    // [1]전체 개수 - 매번 계산하지 않도록 참조변수로!
+    // 로컬스 데이터 변수할당하기!
+    const baseData = JSON.parse(localStorage.getItem("board-data"));
+
+    // [ 상태관리 변수 ] ///
+    // [1] 페이지 번호
+    const [pageNum, setPageNum] = useState(1);
+    // [2] 기능모드
+    const [mode, setMode] = useState("L");
+    // (1) 리스트 모드(L) : List Mode
+    // (2) 글보기 모드(R) : Read Mode
+    // (3) 글쓰기 모드(W) : Write Mode
+    // (4) 수정 모드(M) : Modify Mode (삭제포함)
+
+    // [ 참조변수 ] ///
+    // [1] 전체 개수 - 매번 계산하지 않도록 참조변수로!
     const totalCount = useRef(baseData.length);
     console.log("전체개수:", totalCount);
+
+    // [2] 선택 데이터 저장
+    const selRecord = useRef(null);
+    // -> 특정
+
     // 페이지당 개수
-    const pglength = 9;
+    const unitSize = 8;
 
-    /********************************************************** 
+    /********************************************** 
         함수명: bindList
-        기능: 페이지별 리스트를 생성하여 바인딩함
-    **********************************************************/
+        기능 : 페이지별 리스트를 생성하여 바인딩함
+    **********************************************/
     const bindList = () => {
-        console.log(baseData);
+        // console.log(baseData);
 
-        // 1. 전체 데이터 선택
-        const originalData = baseData;
+        // 1. 전체 원본데이터 선택
+        let orgData = baseData;
+
         // 2. 정렬 적용하기 : 내림차순
-        originalData.sort((a, b) =>
+        orgData.sort((a, b) =>
             Number(a.idx) > Number(b.idx)
                 ? -1
                 : Number(a.idx) < Number(b.idx)
                 ? 1
                 : 0
         );
-        // 3. 일부 데이터만 선택
-        // 예시로 0번부터 9번까지
-        // 한페이지당 10개라면...
-        // 페이지번호와 연관시켜 본다!
-        // 1,2,3,4...
-        // 시작번호(시작)=(페이지번호-1)*페이지당개수
-        let sNum = (pgNum - 1) * pglength;
-        // 끝번호(한계)=(페이지번호-1)*페이지당개수
-        let eNum = pgNum * pglength;
 
-        console.log("첫번호:",sNum,"/끝번호",eNum);
-        // 결과 배열
-        let selData = [];
+        // 3. 일부 데이터만 선택
+        // 예시로 0번부터 9번까지만 선택
+        // 한페이지당 10개라면...
+        // 페이지 번호와 연관시켜 본다!
+        // 1,2,3,4,...
+
+        // 시작번호 = (페이지번호-1)*단위수
+        let sNum = (pageNum - 1) * unitSize;
+        // 끝번호 = 페이지번호*단위수
+        let eNum = pageNum * unitSize;
+        console.log("첫번호:", sNum, "/끝번호:", eNum);
+        // 결과배열
+        const selData = [];
+
         // for문으로 배열 만들기
         for (let i = sNum; i < eNum; i++) {
+            console.log(i);
             // 끝번호가 전체 개수보다 크면 나가라!
-            if(i>=totalCount.current) break;// for문에서 break; 쓰면 나가기임.
-            // for(시작;한계;증감)
-            selData.push(originalData[i]); //selData 에 넣어라 originalData[i]를.
-        }
-        console.log(selData);
+            if (i >= totalCount.current) break;
+            // 대상배열값 추가
+            selData.push(orgData[i]);
+        } ///// for //////
+
+        console.log("일부데이터:", selData);
 
         return selData.map((v, i) => (
             <tr key={i}>
-                {/* 시작번호(sNum)을 더하여 페이지별 순번을 변경 */}
+                {/* 시작번호를 더하여 페이지별 순번을 변경 */}
                 <td>{i + 1 + sNum}</td>
                 <td>
-                    <a href="#" data-idx="51">
+                    <a
+                        href="#"
+                        onClick={(e) => {
+                            e.preventDefault(); //읽기모드로 변경
+                            setMode("R"); //읽기모드로 변경!
+                            selRecord.current = v; // 해당 데이터 저장하기
+                        }}
+                    >
                         {v.cont}
                     </a>
                 </td>
@@ -84,11 +113,8 @@ export default function Board() {
                 <td>{v.cnt}</td>
             </tr>
         ));
-    }; /////////////bindList함수/////////////////
-    /* ***********************************************
-        함수명: pagingList
-        기능: 게시판 리스트의 페이징 기능 목록
-    *********************************************** */
+    }; /////////// bindList 함수 /////////////////
+
     /****************************************** 
     함수명 : pagingList
     기능 : 게시판 리스트의 페이징 기능 목록
@@ -98,11 +124,11 @@ export default function Board() {
         // 유의점: 나머지가 있는지 검사해서 있으면 +1
 
         // 1. 페이징 개수
-        let pagingCount = Math.floor(totalCount.current / pglength);
+        let pagingCount = Math.floor(totalCount.current / unitSize);
 
         // 나머지가 있으면 다음 페이지가 필요함!
         // 나머지가 0이 아니면 1더하기
-        if (totalCount.current % pglength > 0) {
+        if (totalCount.current % unitSize > 0) {
             pagingCount++;
         }
 
@@ -110,7 +136,7 @@ export default function Board() {
             "페이징개수:",
             pagingCount,
             "나머지개수:",
-            totalCount.current % pglength
+            totalCount.current % unitSize
         );
 
         // 링크코드 만들기 ///
@@ -122,7 +148,7 @@ export default function Board() {
                 <Fragment key={i}>
                     {
                         // 페이징번호와 현재페이지번호 일치시 b요소
-                        i === pgNum ? (
+                        i === pageNum ? (
                             <b>{i}</b>
                         ) : (
                             // 불일치시에 모드 링크코드
@@ -130,7 +156,7 @@ export default function Board() {
                                 href="#"
                                 onClick={(e) => {
                                     e.preventDefault();
-                                    setPgNum(i);
+                                    setPageNum(i);
                                 }}
                             >
                                 {i}
@@ -147,56 +173,163 @@ export default function Board() {
         return pgCode;
     }; ////////// pagingList 함수 //////////////
 
+    // 버튼 클릭시 변경함수//////////
+    const clickButton = (e) => {
+        // 버튼 글자 읽기
+        let btnText = e.target.innerText; //버튼은 하위가 없으면 target을 써도 됨(원래는 currenttarget)
+        console.log(btnText);
+        // 버튼별 분기
+        switch (btnText) {
+            case "Write":
+                console.log("글써라!");
+                break;
+            case "List":
+                setMode("L");
+                break;
+         
+        }
+    }; //////////clickButton //////////////
+
     //// 코드 리턴구역 //////////////
     return (
-        <>
-            <main className="cont">
-                <h1 className="tit">OPINION</h1>
-                <div className="selbx">
-                    <select name="cta" id="cta" className="cta">
-                        <option value="tit">Title</option>
-                        <option value="cont">Contents</option>
-                        <option value="unm">Writer</option>
-                    </select>
-                    <select name="sel" id="sel" className="sel">
-                        <option value="0">Descending</option>
-                        <option value="1">Ascending</option>
-                    </select>
-                    <input id="stxt" type="text" maxLength="50" />
-                    <button className="sbtn">Search</button>
-                </div>
-                <table className="dtbl" id="board">
-                    <thead>
-                        <tr>
-                            <th>Number</th>
-                            <th>Title</th>
-                            <th>Writer</th>
-                            <th>Date</th>
-                            <th>Hits</th>
-                        </tr>
-                    </thead>
-                    <tbody>{bindList()}</tbody>
-                    <tfoot>
-                        <tr>
-                            <td colSpan="5" className="paging">
-                                {pagingList()}
-                            </td>
-                        </tr>
-                    </tfoot>
-                </table>
-                <br />
-                <table className="dtbl btngrp">
-                    <tbody>
-                        <tr>
-                            <td>
-                                <button>
-                                    <a href="#">Write</a>
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </main>
-        </>
+        <main className="cont">
+            <h1 className="tit">OPINION</h1>
+            {
+                // 1. 리스트 모드일 경우 리스트 출력하기
+                mode == "L" && (
+                    <ListMode bindList={bindList} pagingList={pagingList} />
+                )
+            }
+            {
+                // 2. 읽기 모드일 경우 리스트 출력하기
+                mode == "R" && <ReadMode selRecord={selRecord} />
+            }
+            <br />
+            <table className="dtbl btngrp">
+                <tbody>
+                    <tr>
+                        <td>
+                            {
+                                //1. 글쓰기 버튼은 로그인상태이고, "L"이면 출력
+                                mode == "L" && (
+                                    <button onClick={clickButton}>Write</button>
+                                )
+                            }
+                            {
+                                //2. 글보기"R" 상태일 경우
+                                mode == "R" && (
+                                    <button onClick={clickButton}>List</button>
+                                )
+                            }
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </main>
     );
 } /////////// Board /////////////////////
+
+/****************************************** 
+        리스트 모드 서브 컴포넌트
+******************************************/
+const ListMode = ({ bindList, pagingList }) => {
+    return (
+        <>
+            <div className="selbx">
+                <select name="cta" id="cta" className="cta">
+                    <option value="tit">Title</option>
+                    <option value="cont">Contents</option>
+                    <option value="unm">Writer</option>
+                </select>
+                <select name="sel" id="sel" className="sel">
+                    <option value="0">Descending</option>
+                    <option value="1">Ascending</option>
+                </select>
+                <input id="stxt" type="text" maxLength="50" />
+                <button className="sbtn">Search</button>
+            </div>
+            <table className="dtbl" id="board">
+                <thead>
+                    <tr>
+                        <th>Number</th>
+                        <th>Title</th>
+                        <th>Writer</th>
+                        <th>Date</th>
+                        <th>Hits</th>
+                    </tr>
+                </thead>
+                <tbody>{bindList()}</tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan="5" className="paging">
+                            {pagingList()}
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        </>
+    );
+}; //////////// ListMode ///////////////////
+
+/****************************************** 
+        읽기 모드 서브 컴포넌트
+******************************************/
+
+const ReadMode = ({ selRecord }) => {
+    // 읽기 모드가 호출되었다는 것은
+    // 리스트의 제목이 클릭되었다는 것을 의미!
+    // 따라서 현재 레코드 값도 저장되었다는 의미!
+    console.log("전달된 참조변수:", selRecord.current);
+    // 전달된 데이터 객체를 변수에 할당
+    const data = selRecord.current;
+
+    return (
+        <>
+            <table className="dtblview readone">
+                <caption>OPINION : Read</caption>
+                <tbody>
+                    <tr>
+                        <td>Name</td>
+                        <td>
+                            <input
+                                type="text"
+                                className="name"
+                                size="20"
+                                readOnly
+                                value={data.unm}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Title</td>
+                        <td>
+                            <input
+                                type="text"
+                                className="subject"
+                                size="60"
+                                readOnly
+                                value={data.tit}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Content</td>
+                        <td>
+                            <textarea
+                                className="content"
+                                cols="60"
+                                rows="10"
+                                readOnly
+                                value={data.cont}
+                            ></textarea>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Attachment</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    );
+}; //////ReadMode///////////
